ab9a69b888f50007e44bbf257341cae0f4683882
Michael Bridgen <mikeb@squaremobius.net>
2012-06-18 11:23:13 +0100

    Make some progress on constructing frames. Wrap the underlying stream rather than trying to be streamy. Ultimately it's the multiplexed SPDY streams that I'll want to present as Stream, with the SPDY flow control somehow mapped through to the pause/resume/drain interface.

--- index.js
+++ index.js
@@ -1,31 +1,116 @@
-var SWEEM = require('sweem'),
-    BS = require('bitsyntax');
+var pattern = require('bitsyntax').compile,
+constructor = require('bitsyntax').constructor,
+EventEmitter = require('events').EventEmitter;
 
-function streamParser(stream, pattern) { // must have rest/binary
-  var parse = BS.compile(pattern);
-  var data = [];
-  return SWEEM(stream).frames(function(accum, newdata) {
-    var data;
-    var frames = [];
+// All SPDY drafts <= 3 have the same framing.
+var parseFrame = pattern('header:32, ' +
+                         'flags:8, length:24, ' +
+                         'data:length/binary, rest/binary');
 
-    if (accum.length === 0) { data = newdata }
-    else if (newdata.length === 0) { data = accum }
-    else {
-      data = new Buffer(accum.length + newdata.length);
-      accum.copy(data, 0);
-      newdata.copy(data, accum.length);
-    }
-    var bindings = parse(data);
-    while (bindings) {
-      frames.push(bindings);
-      data = bindings.rest;
-      bindings = parse(data);
-    }
+function SPDYParser(stream) {
+  this.stream = stream;
+  this.accum = new Buffer(0);
 
-    return (frames.length > 0) ?
-      {value: frames, rest: data} :
-      {rest: data};
-  }, []);
+  var self = this;
+  stream.on('data', function(data) {
+    return self.parse(data);
+  });
+  stream.on('end', function() {
+    return self.emit('end');
+  });
+  stream.on('error', function(err) {
+    return self.emit('error', err);
+  });
+  stream.on('close', function() {
+    return self.emit('close');
+  });
+}
+var P = SPDYParser.prototype = new EventEmitter();
+
+// FIXME better to detect an incorrect header (first 32 bits) before
+// interpreting anything as the length. This won't matter for data
+// frames, which have no distinguishing features other than 0 in the
+// highest bit, but it might help with pathological control(-looking)
+// frames.
+P.parse = function(data0) {
+  var accum = this.accum;
+
+  if (data0.length === 0) { return true; }
+
+  var data;
+  if (accum.length === 0) {
+    data = data0;
+  }
+  else {
+    data = new Buffer(accum.length + data0.length);
+    accum.copy(data, 0);
+    data0.copy(data, accum.length);
+  }
+
+  var frame = parseFrame(data);
+  while (frame) {
+    data = frame.rest;
+    this.emitFrame(frame);
+    frame = parseFrame(data);
+  }
+  this.accum = data;
+  return true;
 }
 
-exports.parser = streamParser;
+P.emitFrame = function(frame) {
+  var header = frame.header;
+  // is the top bit set? (header is 32 bits)
+  if (header > 0x80000000) {
+    var version = (header & 0x7fff0000) / 0x10000;
+    var type = header & 0x0000ffff;
+    return this.emit('controlFrame', {version: version,
+                                      type: type,
+                                      flags: frame.flags,
+                                      data: frame.data});
+  }
+  else {
+    var streamId = header & 0x7fffffff;
+    return this.emit('dataFrame', {streamId: streamId,
+                                   flags: frame.flags,
+                                   data: frame.data});
+  }
+};
+
+var controlFrame = constructor('header:16, type:16,' +
+                               'flagsAndLength:32, data/binary');
+var dataFrame = constructor('header:32, flagsAndLength:32, data/binary');
+
+P.writeControl = function(version, type, flags, data) {
+  // Interpreters can do better than this
+  if (version > 0x8000) {
+    throw "Version out of bounds; must fit in 15 bits";
+  }
+  if (type > 0xffff) {
+    throw "Type out of bounds; must fit in 16 bits";
+  }
+  var length = data.length;
+  if (length > 0xffffff) {
+    throw "Data too large; size must be <= 0xffffff";
+  }
+  var header = 0x8000 + version;
+  return this.stream.write(
+    controlFrame({header: header, type: type,
+                  flagsAndLength: flags * 0x1000000 + length,
+                  data: data}));
+};
+
+P.writeData = function(streamId, flags, data) {
+  if (streamId > 0x80000000) {
+    throw "StreamId out of range (must be 31 bits integer): " + streamId;
+  }
+  var length = data.length;
+  if (length > 0xffffff) {
+    throw "Data too large; size must be <= 0xffffff";
+  }
+  return this.stream.write(
+    dataFrame({header: streamId,
+               flagsAndLength: flags * 0x1000000 + length,
+               data: data}));
+};
+
+exports.SPDYParser = SPDYParser;
